d4091e25a85c5ddb76fd692d1efd30bef5465e83
max <mxpetrovsky@gmail.com>
2021-12-26 19:55:26 +0300

    added start_mario

---
+++ start_mario.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python mario_server.py &
+python mario_server.py -p 5001 &
+python mario_server.py -p 5002 &
+python mario_server.py -p 5003 &
+python mario_server.py -p 5004 &
+python mario_server.py -p 5005 &
+python mario_server.py -p 5006 &
+python mario_server.py -p 5007 &
+python mario_server.py -p 5008 &
+python mario_server.py -p 5009 &
