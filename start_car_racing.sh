5b9e2d531c1a46a073c1cb263b8f9427c7df519d
max <mxpetrovsky@gmail.com>
2022-03-05 15:50:29 +0300

    car racing

---
+++ start_car_racing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python car_racing_server.py &
+python car_racing_server.py -p 5001 &
+python car_racing_server.py -p 5002 &
+python car_racing_server.py -p 5003 &
+python car_racing_server.py -p 5004 &
+python car_racing_server.py -p 5005 &
+python car_racing_server.py -p 5006 &
+python car_racing_server.py -p 5007 &
+python car_racing_server.py -p 5008 &
+python car_racing_server.py -p 5009 &
