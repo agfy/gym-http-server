fdb5b29f371e7da7f637126ee44e8db4425b23b8
max <my@email>
2023-02-05 19:34:32 +0300

    right requrements

--- start_car_racing.sh
+++ start_car_racing.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-python car_racing_server.py &
-python car_racing_server.py -p 5001 &
-python car_racing_server.py -p 5002 &
-python car_racing_server.py -p 5003 &
-python car_racing_server.py -p 5004 &
-python car_racing_server.py -p 5005 &
-python car_racing_server.py -p 5006 &
-python car_racing_server.py -p 5007 &
-python car_racing_server.py -p 5008 &
-python car_racing_server.py -p 5009 &
+python3 car_racing_server.py &
+python3 car_racing_server.py -p 5001 &
+python3 car_racing_server.py -p 5002 &
+python3 car_racing_server.py -p 5003 &
+python3 car_racing_server.py -p 5004 &
+python3 car_racing_server.py -p 5005 &
+python3 car_racing_server.py -p 5006 &
+python3 car_racing_server.py -p 5007 &
+python3 car_racing_server.py -p 5008 &
+python3 car_racing_server.py -p 5009 &
